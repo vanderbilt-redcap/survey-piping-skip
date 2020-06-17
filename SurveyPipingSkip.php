b529835ebf073adb2303518c5f29eb1e05adb6dd
moorejr5 <james.r.moore@vanderbilt.edu>
2020-06-16 19:05:37 -0500

    Debugging.

--- SurveyPipingSkip.php
+++ SurveyPipingSkip.php
@@ -94,7 +94,7 @@ class SurveyPipingSkip extends AbstractExternalModule
                                 var fielddata = dataArray['data'];
                                 console.log(fielddata);
                                 for (fname in metadata) {
-                                    if (fname == name || (fielddata === undefined || fielddata.length == 0)) continue;
+                                    if (fname == name || (fielddata === undefined || dataArray['data'].length == 0)) continue;
                                     var datapoint = '';
                                     if (fname in fielddata) {
                                         datapoint = fielddata[fname];
