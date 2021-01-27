8cec345614081bc66a1ff2a22b239b208a482537
moorejr5 <james.r.moore@vumc.org>
2021-01-27 09:47:45 -0600

    Remove "DateTime" reference from the module's namespace.

--- SurveyPipingSkip.php
+++ SurveyPipingSkip.php
@@ -512,7 +512,7 @@ class SurveyPipingSkip extends AbstractExternalModule
     }
 
     function validateDate($date,$format='Y-m-d') {
-        $d = DateTime::createFromFormat($format, $date);
+        $d = \DateTime::createFromFormat($format, $date);
         // The Y ( 4 digits year ) returns TRUE for any integer with any number of digits so changing the comparison from == to === fixes the issue.
         return $d && $d->format($format) === $date;
     }
