77865aeff6f5837865394a3a258b8f50636aea68
moorejr5 <james.r.moore@vumc.org>
2021-03-16 13:38:31 -0500

    Better check for arrays being the same from one Piping to another.

--- SurveyPipingSkip.php
+++ SurveyPipingSkip.php
@@ -108,7 +108,7 @@ class SurveyPipingSkip extends AbstractExternalModule
                                 var metadata = dataArray['field_types'];
                                 //console.log(metadata);
                                 var fielddata = dataArray['data'];
-                                //console.log('Field Data: '+fielddata);
+                                //console.log(fielddata);
                                 if (!arraysEqual(fielddata,lastFieldData)) {
                                     for (fname in metadata) {
                                         if (fname == name) continue;
@@ -165,19 +165,10 @@ class SurveyPipingSkip extends AbstractExternalModule
                         });
                     });
                     function arraysEqual(a, b) {
-                      if (a === b) return true;
-                      if (a == null || b == null) return false;
-                      if (a.length !== b.length) return false;
-                    
-                      // If you don't care about the order of the elements inside
-                      // the array, you should sort both arrays here.
-                      // Please note that calling sort on an array will modify that array.
-                      // you might want to clone your array first.
-                    
-                      for (var i = 0; i < a.length; ++i) {
-                        if (a[i] !== b[i]) return false;
-                      }
-                      return true;
+                      return Array.isArray(a) &&
+                            Array.isArray(b) &&
+                            a.length === b.length &&
+                            a.every((val, index) => val === b[index]);
                     }
                 </script>";
             }
